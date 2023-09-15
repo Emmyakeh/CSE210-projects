7ff442153a767e25a8c4a0e3121c5fae43d9b85e
Emmyakeh <akehemmy@gmail.com>
2023-09-14 22:38:48 -0600

    Assignment1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string result = "";
+
+        if (number >= 90 )
+        {  result = "A";
+        }
+
+        else if (number >= 80) 
+        { 
+            result = "B";
+
+        }
+
+         else if (number >= 70)
+        { 
+            result = "C";
+
+        }
+
+        else if (number > 60)
+        { 
+            result = "D";
+
+        }
+
+        else 
+
+        {
+            result = "F";
+        }
+            Console.WriteLine($"Your grade is: {result}");
+        
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
     }
 }
