ab2eb1a835b0ac00ec7f8fa1c231cfd3768bff40
Emmyakeh <akehemmy@gmail.com>
2023-10-21 13:51:34 -0600

    update

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -11,23 +11,23 @@ class Program
 
         while (!sampleScripture.AllWordsHidden())
         {
-            Console.Clear(); // Clear the console
+            Console.Clear(); 
 
-            // Display the scripture with hidden words
+            
             Console.WriteLine(sampleScripture.GetCurrentOutput());
 
             if (!sampleScripture.AllWordsHidden())
             {
                 Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
 
-                // Read user input
+                
                 string userInput = Console.ReadLine();
 
-                // Check if the user wants to quit
+                
                 if (userInput.ToLower() == "quit")
                     break;
 
-                // Hide a random word
+                
                 sampleScripture.HideRandomWord();
             }
         }
